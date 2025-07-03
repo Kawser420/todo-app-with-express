559d685459b8bc2a4bfdcc38912bc37519a46a7d
Kawser420 <muhammadkawser202@gmail.com>
2025-07-03 17:08:42 +0600

    created File Stracture

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 import express, { Application, Request, Response } from "express";
 const app: Application = express();
-const port = 3000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  res.send("Hello World! and created express server");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+// [app]-[express.json()]-[todosRouter]-[Root Route "/"]-[GET "/todos"]-[POST Create ToDo]
+//[todosRouter]-[get all todos /todos GET]-[create todo /todos/create-todo POST todo]
+
+export default app;
+
+/**
+ * Basic File structure
+ * server - server handling like - starting, closing error handling of server. only related to server
+ * app file - routing handle, middleware, route related error
+ * app folder - app business logic handling like create read update delete, database related works
+ */
