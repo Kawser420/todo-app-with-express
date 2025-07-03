97df3ed8ca45be6f7a13d1f5d5d359a31a8791ee
Kawser420 <muhammadkawser202@gmail.com>
2025-07-03 17:29:49 +0600

    node server file is ready

--- dist/app.js
+++ dist/app.js
@@ -5,10 +5,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-const port = 3000;
 app.get("/", (req, res) => {
     res.send("Hello World! and created express server");
 });
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+// [app]-[express.json()]-[todosRouter]-[Root Route "/"]-[GET "/todos"]-[POST Create ToDo]
+//[todosRouter]-[get all todos /todos GET]-[create todo /todos/create-todo POST todo]
+exports.default = app;
+/**
+ * Basic File structure
+ * server - server handling like - starting, closing, error handling of server. only related to server
+ * app file - routing handle, middleware, route related error
+ * app folder - app business logic handling like create read update delete, database related works
+ */
--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ export default app;
 
 /**
  * Basic File structure
- * server - server handling like - starting, closing error handling of server. only related to server
+ * server - server handling like - starting, closing, error handling of server. only related to server
  * app file - routing handle, middleware, route related error
  * app folder - app business logic handling like create read update delete, database related works
  */
