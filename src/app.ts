67a91e0aa5400a00236979484881e8b4541ecc71
Kawser420 <muhammadkawser202@gmail.com>
2025-07-11 00:01:15 +0600

    file stracture fix and mantance the project

--- src/app.ts
+++ src/app.ts
@@ -32,7 +32,7 @@ export default app;
 
 /**
  * Basic File Structure
- * server - server handling like - starting, closing, error handling of server. only related to server
- * app file - routing handle, middleware, route related error
+ * Server - server handling like - starting, closing, error handling of server. only related to server
+ * App File - routing handle, middleware, route related error
  * app folder - app business logic handling like create read update delete, database related works
  */
